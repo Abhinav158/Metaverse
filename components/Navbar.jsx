7cb197888d3c953c4380d44c779accb6dc5be5ef
Abhinav <raghunandan.abhinav@gmail.com>
2022-11-26 09:22:45 +0530

    Aded navbar and cover page section

---
+++ components/Navbar.jsx
@@ -0,0 +1,28 @@
+// All server components inside app directory are React Server Components by default 
+// We do not we to use const getServerSideProps, etc.
+//We can automatically adopt server components without doing any extra work
+
+'use client';
+
+//Include the style and motion files using Framer 
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { navVariants } from '../utils/motion';
+
+
+const Navbar = () => (
+  <motion.nav variants={navVariants} initial="hidden" whileInView="show"
+  className={`${styles.xPaddings} py-8 relative`}>
+    <div className="absolute w-[50%] inset-0 gradient-01">
+    </div>
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+      <img src="/search.svg" alt="search" className="w-[24px] h-[24px] object-contained"></img>
+      <h2 className="font-extrabold text-[24px] leading-30px text-white">
+        EXPLORE VIRTUAL REALITY
+      </h2>
+      <img src='./menu.svg' alt="menu" className='w-[24px] h-[24px] object-contain' />
+    </div>
+  </motion.nav>
+);
+
+export default Navbar;
---
+++ sections/About.jsx
@@ -0,0 +1,14 @@
+'use client';
+
+//Include the style and motion files using Framer 
+import { motion, MotionConfig } from 'framer-motion';
+import styles from '../styles';
+import { slideIn, staggerContainer, textVariant } from '../utils/motion';
+
+const About = () => (
+  <section>
+    About section
+  </section>
+);
+
+export default About;
